22bf18946ea2e18a164f1538e0a38c6f656d0800
elisheba-beep <ekunife35@gmail.com>
2023-07-01 09:17:54 +0100

    welcome screen added with navigation

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import WelcomeScreen from "./src/screens/WelcomeScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import LoginScreen from "./src/screens/LoginScreen";
+import RegisterScreen from "./src/screens/RegisterScreen";
+
+// stack
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer style={styles.container}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="LogIn"
+            component={LoginScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Register"
+            component={RegisterScreen}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+        <StatusBar style="auto" />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,84 @@
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function LoginScreen({ navigation }) {
+  return (
+    <ImageBackground
+      accessibility={true}
+      accessibilityLabel="Login Screen Background Image"
+      style={styles.background}
+      source={require("../img/loginBackground.png")}
+    >
+      <SafeAreaView style={styles.container}>
+        <View></View>
+        <View>
+          <Text style={styles.title}>RECI</Text>
+          <Text style={styles.subTitle}>COOK IT YOURSELF</Text>
+        </View>
+        <View style={styles.buttonContainer}>
+          <Pressable
+            onPress={() => navigation.navigate("Register")}
+            style={styles.registerButton}
+          >
+            <Text style={styles.buttonText}>REGISTER</Text>
+          </Pressable>
+          <Pressable
+            onPress={() => navigation.navigate("LogIn")}
+            style={styles.loginButton}
+          >
+            <Text style={styles.buttonText}>LOG IN</Text>
+          </Pressable>
+        </View>
+      </SafeAreaView>
+    </ImageBackground>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: 30,
+  },
+  background: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 76.5,
+    color: "#F3F3F3",
+  },
+  subTitle: {
+    fontSize: 16,
+    color: "#F3F3F3",
+  },
+  registerButton: {
+    backgroundColor: "#F26419",
+    borderRadius: 6,
+    padding: 5,
+    marginVertical: 20,
+  },
+  loginButton: {
+    backgroundColor: "transparent",
+    borderRadius: 6,
+    padding: 5,
+    borderWidth: 2,
+    borderColor: "#F26419",
+    marginVertical: 20,
+  },
+  buttonContainer: {
+    width: "100%",
+  },
+  buttonText: {
+    color: "#EAEAEA",
+    fontSize: 24,
+    textAlign: "center",
+  },
+});
