5d9524e566be573c3ff52d917435efab6e2791b8
elisheba-beep <ekunife35@gmail.com>
2023-07-04 13:54:32 +0100

    trending list update

--- src/utils/trending.js
+++ src/utils/trending.js
@@ -1,34 +1,26 @@
 export const trending = [
-    {
-      url: require("../img/pizza.png"),
-      title: "Homemade Pizza",
-    },
-    {
-      url: require("../img/cake.png"),
-      title: "Chocolate Cake",
-    },
-    {
-      url: require("../img/cake.png"),
-      title: "Chocolate Cake",
-    },
-    {
-      url: require("../img/pizza.png"),
-      title: "Homemade Pizza",
-    },
-    {
-      url: require("../img/cake.png"),
-      title: "Chocolate Cake",
-    },
-    {
-      url: require("../img/pizza.png"),
-      title: "Homemade Pizza",
-    },
-    {
-      url: require("../img/cake.png"),
-      title: "Chocolate Cake",
-    },
-    {
-      url: require("../img/pizza.png"),
-      title: "Homemade Pizza",
-    },
-  ];
+  {
+    url: require("../img/pizza.png"),
+    title: "Homemade Pizza",
+  },
+  {
+    url: require("../img/cake.png"),
+    title: "Chocolate Cake",
+  },
+  {
+    url: require("../img/cake.png"),
+    title: "Chocolate Cake",
+  },
+  {
+    url: require("../img/pizza.png"),
+    title: "Homemade Pizza",
+  },
+  {
+    url: require("../img/pizza.png"),
+    title: "Homemade Pizza",
+  },
+  {
+    url: require("../img/cake.png"),
+    title: "Chocolate Cake",
+  },
+];
